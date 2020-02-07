70389dceb8d59f38049b4524203cdc2dc6eca6cf
GravyHTX <godisgravy@gmail.com>
2020-02-06 23:23:46 -0600

    Console log displays correctly changed values however app is not functionable. See README for more information.

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # SPG
 Super Password Generator v2.0
+
+This is a random password generator based on a series of promts that determine requirements of creating a secure password.
+
+To begin click on "let's go" and the user will encounter a series of prompts
+
+The prompts are Boolean (true/false) statements for the variables:
+    -  Lowercase letters
+    -  Uppercase letters
+    -  Numbers
+    -  Symbols
+
+The final prompt will query a number between 8-128 which will determine the character amount.
+
+Arrays are set up for all possible options of each variable in which the character length will determine how many times it randomly chooses a value within the "true" arrays.
+
+The final result will give the password based on the user requirements.
+
+This process can be repeated as many times as user wishes by clicking "let's go" to generate a new secure password.
+
+
+
+NOTE: Console properly displays prompt responses, however randomization based on number of characters is incomplete. This program will not function as expected.
--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var numChoices = [0,1,2,3,4,5,6,7,8,9];
 var lcChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var ucChoices = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numChoices = [0,1,2,3,4,5,6,7,8,9];
 var symbChoices = ["!", "#", "$", "%", "&", "*", "+", "-", "/", ":", ";", "<", ">", "=", "?", "_", "`", "~", "^", "@", ",", "(", ")", "{", "|", "}"];
 
+var ans = ["y", "n"]
+var inc = false;
 
-var charSet = [ // 4 array sets [0] [1] [2] [3]
-  [true, "Lowercase"], // these are arrays [0] [1] [2]
-	[true, "Numbers"],
-  [true, "Uppercase"],
-	[true, "Symbols"],
-];
+var lowerCase = false;
+var upperCase = false;
+var numbers = false;
+var symbols = false;
+var charNum = 0;
+
+// var lowerCase = prompt ("Does your password need NUMBERS? (y/n)")
+// lowerCase = lowerCase.toLowerCase();
+
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -23,20 +30,103 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", letsGo);
 
 
+// writePassword
 
 
 
 function generatePassword () {
 // change "placeholder" value using "setAttribute"
+// );
 
 
 }
 
 
-// onclick   alert("パスワード生成ツールへようこそ、ご主人様！ Click to continue...");
+function letsGo () {
+
+  var lc = prompt("Does your password need LOWERCASE? (y/n)");
+  lc = lc.toLowerCase();
+  if (lc === "y" || lc === "n"){
+    lowerCase = !lowerCase;
+    if (lc === "y") {
+      if (lc === "y");
+      var lowerCase = true;
+      console.log(lowerCase); 
+    }
+    else if (lc === "n") {
+
+      var lowerCase = false;
+      console.log(lowerCase)
+    }
+      
+  }
+   
+  var uc = prompt("Does your password need UPPERCASE? (y/n)");
+  uc = uc.toLowerCase();
+  if (uc === "y" || uc === "n"){
+    upperCase = !upperCase;
+    if (uc === "y") {
+      if (uc === "y");
+      var upperCase = true;
+      console.log(upperCase); 
+    }
+    else if (uc === "n") {
+
+      var upperCase = false;
+      console.log(upperCase)
+    }
+      
+  }
+
+  var nm = prompt("Does your password need NUMBERS? (y/n)");
+  nm = nm.toLowerCase();
+  if (nm === "y" || nm === "n"){
+    numbers = !numbers;
+    if (nm === "y") {
+      if (nm === "y");
+      var numbers = true;
+      console.log(numbers); 
+    }
+    else if (nm === "n") {
+
+      var numbers = false;
+      console.log(numbers)
+    }
+      
+  }
+
+  var sb = prompt("Does your password need SYMBOLS? (y/n)");
+  sb = sb.toLowerCase();
+  if (sb === "y" || sb === "n"){
+    symbols = !symbols;
+    if (sb === "y") {
+      if (sb === "y");
+      var symbols = true;
+      console.log(symbols); 
+    }
+    else if (sb === "n") {
+
+      var symbols = false;
+      console.log(symbols)
+    }
+      
+  }
+
+  var ch = prompt("How many characters does your password need? (8-128)");
+    if (ch <= 128 || ch >= 8){
+    var charNum = ch;
+    console.log(charNum)
+    }
+    else {
+      ch = prompt();
+    }
+
+  
+
+}
 
 // Does your password need NUMBERS? (y/n)
     // onlyLowerCase .. storevalue for charSet
@@ -92,3 +182,49 @@ function generatePassword () {
   
 
 
+
+
+  // var charSet = [ // 4 array sets [0] [1] [2] [3]
+//   [true, "Lowercase"], // these are arrays [0] [1]
+// 	[true, "Numbers"],
+//   [true, "Uppercase"],
+// 	[true, "Symbols"],
+
+
+// Lowercase checkbox
+// lowerCase.addEventListener( "change", function() {
+//   if(lowerCase.checked) {
+//       console.log("l")
+//   } else {
+//       console.log(clearInterval(lowerCase))
+//   }
+// });
+
+// // Uppercase checkbox
+
+// upperCase.addEventListener( "change", function() {
+//   if(upperCase.checked) {
+//       console.log("u")
+//   } else {
+//       console.log(clearInterval(upperCase))
+//   }
+// });
+
+// // Numbers checkbox
+
+// numbers.addEventListener( "change", function() {
+//   if(numbers.checked) {
+//       console.log("n")
+//   } else {
+//       console.log(clearInterval(numbers))
+//   }
+// });
+
+// // Symbols checkbox
+// // document.getElementById("symbols").addEventListener("click", displayCheck);
+// symbols.addEventListener( "change", function() {
+//   if(symbols.checked) {
+//       console.log("s")
+//   } else {
+//       console.log(clearInterval(symbols))
+//   }
