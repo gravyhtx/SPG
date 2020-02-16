d24d8bd0d58b4a45b137694829d951d87cc73cae
GravyHTX <godisgravy@gmail.com>
2020-02-15 19:08:47 -0600

    omg its working

--- script.js
+++ script.js
@@ -1,132 +1,286 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var lcChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var ucChoices = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numChoices = [0,1,2,3,4,5,6,7,8,9];
-var symbChoices = ["!", "#", "$", "%", "&", "*", "+", "-", "/", ":", ";", "<", ">", "=", "?", "_", "`", "~", "^", "@", ",", "(", ")", "{", "|", "}"];
 
-var ans = ["y", "n"]
-var inc = false;
+
+
+// var pwChoices = [lcChoices, ucChoices, numChoices, symbChoices]
 
 var lowerCase = false;
 var upperCase = false;
 var numbers = false;
 var symbols = false;
-var charNum = 0;
-
-// var lowerCase = prompt ("Does your password need NUMBERS? (y/n)")
-// lowerCase = lowerCase.toLowerCase();
 
 
+var lcChoices;
+var ucChoices;
+var numChoices;
+var symbChoices;
+var CHARACTER_SETS = [
+	[false, "Numbers", "0123456789"],
+	[false, "Lowercase", "abcdefghijklmnopqrstuvwxyz"],
+	[false, "Uppercase", "ABCDEFGHIJKLMNOPQRSTUVWXYZ"],
+	[false, "ASCII symbols", "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"],
+	[false, "Space", " "],
+];
 
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
 
-}
+pw = false;
+console.log("pw is " + pw)
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", letsGo);
 
-
-// writePassword
-
-
-
-function generatePassword () {
-// change "placeholder" value using "setAttribute"
-// );
-
-
-}
-
-
 function letsGo () {
+
+  // var length = parseInt(document.getElementById("length").value, 10);
+	// else if (document.getElementById("by-entropy").checked)
+	// 	length = Math.ceil(parseFloat(document.getElementById("entropy").value) * Math.log(2) / Math.log(charset.length));
+	// else
 
   var lc = prompt("Does your password need LOWERCASE? (y/n)");
   lc = lc.toLowerCase();
-  if (lc === "y" || lc === "n"){
-    lowerCase = !lowerCase;
-    if (lc === "y") {
-      if (lc === "y");
-      var lowerCase = true;
-      console.log(lowerCase); 
-    }
-    else if (lc === "n") {
+  if (lc === "n") {
+    var lowerCase = false;
+    console.log("'lowerCase' is " + lowerCase);
+  }
 
-      var lowerCase = false;
-      console.log(lowerCase)
-    }
-      
+  else {
+
+    var lowerCase = true;
+    console.log("'lowerCase' is " + lowerCase);
   }
    
   var uc = prompt("Does your password need UPPERCASE? (y/n)");
   uc = uc.toLowerCase();
-  if (uc === "y" || uc === "n"){
-    upperCase = !upperCase;
-    if (uc === "y") {
-      if (uc === "y");
-      var upperCase = true;
-      console.log(upperCase); 
-    }
-    else if (uc === "n") {
+  if (uc === "n") {
+    var upperCase = false;
+    console.log("'upperCase' is " + upperCase);
+  }
 
-      var upperCase = false;
-      console.log(upperCase)
-    }
-      
+  else {
+    var upperCase = true;
+    console.log("'upperCase' is " + upperCase);
   }
 
   var nm = prompt("Does your password need NUMBERS? (y/n)");
   nm = nm.toLowerCase();
-  if (nm === "y" || nm === "n"){
-    numbers = !numbers;
-    if (nm === "y") {
-      if (nm === "y");
-      var numbers = true;
-      console.log(numbers); 
-    }
-    else if (nm === "n") {
+  if (nm === "n") {
+    var numbers = false;
+    console.log("'numbers' is " + numbers);
+  }
 
-      var numbers = false;
-      console.log(numbers)
-    }
-      
+  else {
+    var numbers = true;
+    console.log("'numbers' is " + numbers);
   }
 
   var sb = prompt("Does your password need SYMBOLS? (y/n)");
   sb = sb.toLowerCase();
-  if (sb === "y" || sb === "n"){
-    symbols = !symbols;
-    if (sb === "y") {
-      if (sb === "y");
-      var symbols = true;
-      console.log(symbols); 
-    }
-    else if (sb === "n") {
 
+  if (sb === "n") {
       var symbols = false;
-      console.log(symbols)
-    }
-      
+      console.log("'symbols' is " + symbols);
   }
 
-  var ch = prompt("How many characters does your password need? (8-128)");
-    if (ch <= 128 || ch >= 8){
-    var charNum = ch;
-    console.log(charNum)
-    }
-    else {
-      ch = prompt();
-    }
-
+  else {
+      var symbols = true;
+      console.log("'symbols' is " + symbols);
+  }
   
 
+  var ch = prompt("How many characters does your password need? (8-128)");
+
+  if (ch <= 128 && ch >= 8){
+  var charNum = ch;
+  console.log(charNum);
+  pw = true;
+  console.log("'pw' is " + pw);
+  }
+
+  else {
+    alert("You bastard");
+    var charNum = null;
+    console.log(charNum);
+    pw = false;
+    console.log("'pw' is " + pw);
+    return;
+  }
+  
+  
+
+  function generatePassword () {
+  var password = "";
+  var pwChoices = [];
+    // if (pw == true) 
+    if (lowerCase == true) {
+      pwChoices.push("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",);
+      
+    }
+    if (upperCase == true) {
+      pwChoices.push("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",);
+
+    }
+    if (numbers == true) {
+      pwChoices.push(0,1,2,3,4,5,6,7,8,9,);
+
+    }
+    if (symbols == true) {
+      pwChoices.push("!", "#", "$", "%", "&", "*", "+", "-", "/", ":", ";", "<", ">", "=", "?", "_", "`", "~", "^", "@", ",", "(", ")", "{", "|", "}",);
+    }
+
+    for (var i = 0; i < (charNum); i++) {
+      
+      password += pwChoices[Math.floor(Math.random() * pwChoices.length)];
+
+      // Display in console
+      console.log(password);
+    }
+    
+  
+
+  function writePassword() {
+    // var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
+  
+  }
+
+  
+  writePassword ();
+  
 }
+generatePassword();
+}
+// function genPassword() {
+//   var length = 8,
+//       charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+//       retVal = "";
+//   for (var i = 0, n = charset.length; i < length; ++i) {
+//       retVal += charset.charAt(Math.floor(Math.random() * n));
+//   }
+//   return retVal;
+// }
+
+// console.log(retVal)
+
+
+// function doGenerate(ev) {
+// 	ev.preventDefault();
+// currentPassword = generatePassword(charset, length);
+	
+// 	// Calculate and format entropy
+// 	var entropy = Math.log(charset.length) * length / Math.log(2);
+// 	var entropystr;
+// 	if (entropy < 70)
+// 		entropystr = entropy.toFixed(2);
+// 	else if (entropy < 200)
+// 		entropystr = entropy.toFixed(1);
+// 	else
+// 		entropystr = entropy.toFixed(0);
+	
+// 	// Set output elements
+// 	passwordElem.textContent = currentPassword;
+
+// }
+// function getPasswordCharacterSet() {
+// 	// Concatenate characters from every checked entry
+// 	var rawCharset = "";
+// 	CHARACTER_SETS.forEach(function(entry, i) {
+// 			rawCharset += entry[2];
+	
+// 	// Parse UTF-16, remove duplicates, convert to array of strings
+// 	var charset = [];
+// 	for (var i = 0; i < rawCharset.length; i++) {
+// 		var c = rawCharset.charCodeAt(i);
+// 		if (c < 0xD800 || c >= 0xE000) {  // Regular UTF-16 character
+// 			var s = rawCharset.charAt(i);
+// 			if (charset.indexOf(s) == -1)
+// 				charset.push(s);
+// 			continue;
+// 		}
+// 		if (0xD800 <= c && c < 0xDC00 && i + 1 < rawCharset.length) {  // High surrogate
+// 			var d = rawCharset.charCodeAt(i + 1);
+// 			if (0xDC00 <= d && d < 0xE000) {  // Low surrogate
+// 				var s = rawCharset.substring(i, i + 2);
+// 				i++;
+// 				if (charset.indexOf(s) == -1)
+// 					charset.push(s);
+// 				continue;
+// 			}
+// 		}
+		
+// 	}
+// 	return charset;
+//   }
+
+
+// function generatePassword(charset, len) {
+// 	var result = "";
+// 	for (var i = 0; i < len; i++)
+// 		result += charset[randomInt(charset.length)];
+// 	return result;
+
+
+
+// // Returns a random integer in the range [0, n) using a variety of methods.
+// function randomInt(n) {
+// 	var x = randomIntMathRandom(n);
+// 	x = (x + randomIntBrowserCrypto(n)) % n;
+// 	return x;
+// }
+
+
+// // Not secure or high quality, but always available.
+// function randomIntMathRandom(n) {
+// 	var x = Math.floor(Math.random() * n);
+// 	if (x < 0 || x >= n)
+// 		throw "Arithmetic exception";
+// 	return x;
+// }
+
+  // }
+  // generatePassword (); 
+  // }
+
+
+    // var randomLC = lcChoices[Math.random() * lcChoices.length];
+    // console.log(randomLC)
+    // console.log(lcChoices)
+    
+    
+      // for (var i = 0; i < charNum + 1; i++) {
+      //   console.log("I love " + pw[i]);
+    
+      // }
+    
+               
+  // }
+  
+      
+    
+      // var pwGen = [randomLC, randomUC, randomNum, randomSym]
+      // console.log(pwGen)
+  
+      // var pwChoice = [Math.floor(Math.random() * pwGen.length)]; 
+// }
+    
+//   }
+// }
+// if (pw = true); {
+  
+  
+
+  
+// }
+  // generatePassword();
+// }
+
+
+
 
 // Does your password need NUMBERS? (y/n)
     // onlyLowerCase .. storevalue for charSet
@@ -162,69 +316,3 @@ function letsGo () {
 //  check for truthyness
     // [if [0] is true (always for Lowercase set)... etc]
 
-//give computer
-
-
-
-
-
-// for (var i = 0; i < 10; i++) 
-// var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-    // Randomly chooses a choice from the options array. This is the Computer's guess.
-
-
-  // function randomIntMathRandom(n) {
-  //   var x = Math.floor(Math.random() * n);
-  //   if (x < 0 || x >= n)
-  //     throw "Arithmetic exception";
-  //   return x;
-  // }
-  
-
-
-
-
-  // var charSet = [ // 4 array sets [0] [1] [2] [3]
-//   [true, "Lowercase"], // these are arrays [0] [1]
-// 	[true, "Numbers"],
-//   [true, "Uppercase"],
-// 	[true, "Symbols"],
-
-
-// Lowercase checkbox
-// lowerCase.addEventListener( "change", function() {
-//   if(lowerCase.checked) {
-//       console.log("l")
-//   } else {
-//       console.log(clearInterval(lowerCase))
-//   }
-// });
-
-// // Uppercase checkbox
-
-// upperCase.addEventListener( "change", function() {
-//   if(upperCase.checked) {
-//       console.log("u")
-//   } else {
-//       console.log(clearInterval(upperCase))
-//   }
-// });
-
-// // Numbers checkbox
-
-// numbers.addEventListener( "change", function() {
-//   if(numbers.checked) {
-//       console.log("n")
-//   } else {
-//       console.log(clearInterval(numbers))
-//   }
-// });
-
-// // Symbols checkbox
-// // document.getElementById("symbols").addEventListener("click", displayCheck);
-// symbols.addEventListener( "change", function() {
-//   if(symbols.checked) {
-//       console.log("s")
-//   } else {
-//       console.log(clearInterval(symbols))
-//   }
